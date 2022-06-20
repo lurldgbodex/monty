d0a86cc9a5a4b2c6ea6db58d26896b757ad323b8
Lurldgbodex <professionaleditor6@gmail.com>
2022-06-20 03:01:49 -0700

    update code

--- monty.c
+++ monty.c
@@ -17,13 +17,13 @@ int main(int ac, char *av[])
 
 	if (ac != 2)
 	{
-		fprintf(stderr, "usage: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	fd = fopen(av[1], "r");
 	if (fd == NULL)
 	{
-		fprintf(stderr, "Error: can't open file %s\n", av[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
 

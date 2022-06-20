92bc66bf862ac52a44a0f4f0c3762afdfe7b5170
Lurldgbodex <professionaleditor6@gmail.com>
2022-06-20 02:56:55 -0700

    update code

--- stack.c
+++ stack.c
@@ -36,8 +36,8 @@ void push(stack_t **stack, unsigned int line_number, int data)
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-	(void)line_number;
 	stack_t *temp = *stack;
+	(void)line_number;
 
 	while (temp)
 	{
